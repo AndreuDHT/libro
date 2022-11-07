18255e219534aa6adc57af0a24894da98916c28d
Andreudht <andreudht@gmail.com>
2022-11-07 12:46:56 +0100

    Modificado 1

---
+++ libro/libro.java
@@ -0,0 +1,13 @@
+package libro;
+
+public class libro {
+
+    public static void main(String args[]) {
+
+        System.out.println("Tipos de libros");
+        System.out.println("Libro de texto");
+        System.out.println("Manga");
+        System.out.println("Cómic");
+
+    }
+}
