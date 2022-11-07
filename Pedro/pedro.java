fce66866d49cfcffa3078447eca1bf069b671d0c
Andreudht <andreudht@gmail.com>
2022-11-07 13:24:16 +0100

    Ficheros modificados 2

--- Pedro/pedro.java
+++ Pedro/pedro.java
@@ -1,4 +1,11 @@
 package Pedro;
 
 class pedro {
+
+    public static void main(String args[]) {
+
+        System.out.println("Soy AndreuDHT");
+        System.out.println("Soy Andreudht");
+
+    }
 }
